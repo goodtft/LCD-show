e14a951747d09a00b6e3094786b51fcee841a35e
Bennett-Wendorf <bennettwendorf@gmail.com>
2022-01-09 17:01:41 -0600

    Removed some extra files on system restore and fixed LCD-hdmi script by changing directory.

--- system_restore.sh
+++ system_restore.sh
@@ -27,6 +27,13 @@ fi
 ls -al ./.system_backup/*.dtb > /dev/null 2>&1 && sudo cp -rf ./.system_backup/*.dtb  /boot/overlays/
 ls -al ./.system_backup/*.dtbo > /dev/null 2>&1 && sudo cp -rf ./.system_backup/*.dtbo  /boot/overlays/
 
+if [ -f /usr/share/X11/xorg.conf.d/99-fbdev.conf ]; then
+    sudo rm -rf /usr/share/X11/xorg.conf.d/99-fbdev.conf
+fi
+if [ -f /usr/share/X11/xorg.conf.d/99-fbturbo.conf ]; then
+    sudo rm -rf /usr/share/X11/xorg.conf.d/99-fbturbo.conf
+fi
+
 if [ -f ./.system_backup/99-fbturbo.conf ];then
 sudo cp -rf ./.system_backup/99-fbturbo.conf /usr/share/X11/xorg.conf.d
 fi
